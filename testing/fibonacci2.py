dabb9f9d6dfe3b6a173aa56b1a432bd325d2d7e7
vbevia <vbevia@gmail.com>
2024-11-15 09:34:31 +0100

    add comments

--- testing/fibonacci2.py
+++ testing/fibonacci2.py
@@ -1,6 +1,16 @@
 # find the fibonacci sequence for up to n = 8
 # test for edge cases, for instance n = 1
 
+# the naive recursive Fibonacci function does not use memoization
+# or dynamic programming and hence exhibits exponential complexity.
+#
+# The total number of calls for  \text{find_fibonacci(n)}  grows exponentially because the function recomputes the same values multiple times.
+#
+# For example:
+# 	•	 n = 3 : 5 calls.
+# 	•	 n = 4 : 9 calls.
+# 	•	 n = 5 : 15 calls.
+# 	•	 n = 6 : 29 calls.
 
 def find_fibonacci(n):
     # base case
